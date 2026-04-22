3cbd4c9f8e2d9708fc8b98d90b7aa56723a18a21
greenhornet63603 <greenhornet63603@gmail.com>
2026-04-22 20:45:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,31 +9,31 @@ LOGIN_SYSTEM = bool(os.environ.get('LOGIN_SYSTEM', True)) # True or False
 
 if LOGIN_SYSTEM == False:
     # if login system is False then fill your tg account session below 
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "False")
 else:
     STRING_SESSION = None
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8724093305:AAEQymahtqxrtWLk1gpHibRmU_1rdIrdkfo")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "24310647"))
+API_ID = int(os.environ.get("API_ID", "27400172"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "8e043df44de667fff9ae6aabbcf186f3")
+API_HASH = os.environ.get("API_HASH", "56d0a75c5f9a9de6beb5452aa63c2d36")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "8278569969"))
+ADMINS = int(os.environ.get("ADMINS", "7540570087"))
 
 # Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
 # And Make Your Bot Admin In this channel with full rights.
 # if you don't want to upload in channel then leave it blank don't fill anything.
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "-1003932686149")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://servicepetmania_db_user:z67e3VUswZfNxEgx@petmania.np5tw4n.mongodb.net/?retryWrites=true&w=majority&appName=petmania") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "hunterBOt")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://greenhornet63603:CvnxnjzknPLxYOfo@cluster0.qif4g18.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
 WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) # time in seconds
